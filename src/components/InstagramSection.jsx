2f607def17f45696e760d41803ae3e727e49047e
Junel Amelon <junelamelon92@gmail.com>
2026-04-10 15:34:59 +0100

    UPDATE

--- src/components/InstagramSection.jsx
+++ src/components/InstagramSection.jsx
@@ -1,10 +1,10 @@
 const PHOTOS = [
   'https://amiy.wpenginepowered.com/wp-content/uploads/2023/10/home-1-instagram-1-300x234.webp',
   'https://amiy.wpenginepowered.com/wp-content/uploads/2023/10/home-1-instagram-2-300x234.webp',
-  'https://amiy.wpenginepowered.com/wp-content/uploads/2023/10/home-1-instagram-4-300x234.webp',
-  'https://amiy.wpenginepowered.com/wp-content/uploads/2023/10/home-1-instagram-5-300x234.webp',
-  'https://amiy.wpenginepowered.com/wp-content/uploads/2023/10/home-1-instagram-6-300x234.webp',
-  'https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?auto=format&fit=crop&w=800&q=80',
+  'https://images.pexels.com/photos/29745246/pexels-photo-29745246.jpeg',
+  'https://images.pexels.com/photos/5468639/pexels-photo-5468639.jpeg',
+  'https://images.pexels.com/photos/6663574/pexels-photo-6663574.jpeg',
+  'https://images.pexels.com/photos/9246306/pexels-photo-9246306.jpeg',
 ];
 
 export default function InstagramSection() {
