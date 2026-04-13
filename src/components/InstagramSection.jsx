e81f36411c23b2069348d52b5fe9d7acb49ce7fb
Junel Amelon <junelamelon92@gmail.com>
2026-04-13 10:48:56 +0100

    UPDATE

--- src/components/InstagramSection.jsx
+++ src/components/InstagramSection.jsx
@@ -2,7 +2,7 @@ const PHOTOS = [
   'https://amiy.wpenginepowered.com/wp-content/uploads/2023/10/home-1-instagram-1-300x234.webp',
   'https://images.pexels.com/photos/19101350/pexels-photo-19101350.jpeg',
   'https://images.pexels.com/photos/29745246/pexels-photo-29745246.jpeg',
-  'https://images.pexels.com/photos/5468639/pexels-photo-5468639.jpeg',
+  'https://images.pexels.com/photos/10460940/pexels-photo-10460940.jpeg',
   'https://images.pexels.com/photos/6663574/pexels-photo-6663574.jpeg',
   'https://images.pexels.com/photos/9246306/pexels-photo-9246306.jpeg',
 ];
